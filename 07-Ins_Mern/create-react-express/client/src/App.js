c835c43e79c463f0e551327af4e4915e934d4bcc
tindoann <tindoann@gamil.com>
2019-11-07 14:11:39 -0500

    setup routes in app

--- 07-Ins_Mern/create-react-express/client/src/App.js
+++ 07-Ins_Mern/create-react-express/client/src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
-import HelloBootstrap from "./components/HelloBootstrap";
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+
 
 function App() {
-  return <HelloBootstrap />;
-}
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Wrapper>
+          <Route path="/" exact component={Search} />
+          <Route path="/search" component={Search} />
+          <Route path="/saved" component={Saved} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
