e559af1284819ec876c88702b44e5174d139806e
tindoann <tindoann@gamil.com>
2019-11-08 20:01:55 -0500

    react build

---
+++ 07-Ins_Mern/create-react-express/client/build/precache-manifest.e674b9eb8904a2a129bc73e2747eadca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80226c545eb260ca6fcb77bcff5c5fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9220481c614fb2e009",
+    "url": "/static/js/2.51a75e38.chunk.js"
+  },
+  {
+    "revision": "dfa5e6e8e1fbcdf2549f",
+    "url": "/static/js/main.f40ef50e.chunk.js"
+  },
+  {
+    "revision": "55855338982e403cd313",
+    "url": "/static/js/runtime-main.d80f8818.js"
+  }
+]);
